c20a521e524c18d470078ca7b6ce291de1764106
Novendra619 <154975093+Novendra619@users.noreply.github.com>
2025-08-28 15:32:09 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/SMKN1 Merdeka.png"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI PERPUSTAKAAN <br> SMKN1 MERDEKA</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi Perpustakaan SMKN1 Merdeka
+                            <br>
+                            <br>
+                            SMKN1 Merdeka
+                            <br>
+                           
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ views/kontak_tampil.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>PERPUSTAKAAN ZAHIRA KISARAN</strong></center></label>
+                            <p align="center">
+                              Pendidikan No. 1, Jaranguda, Kec. Merdeka<br>
+                              Kabupaten Karo, Sumatera Utara, Kode Pos : 22156
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. 12345678<br>
+                            Email : bskmerdeka@yaoo.co.id<br>
+                            Fax : 062892113<br>
+                            Website : <a href="https://smkn1merdeka.sch.id">SMKN1 Merdeka<a/>/<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
